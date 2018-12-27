e53a351cc78a25ffabc5b9dfd415e5ebae60dcbc
chunchunhua <1041367524@qq.com>
2018-12-27 19:16:08 +0800

    查看菜单页面

--- src/view/menu/view.vue
+++ src/view/menu/view.vue
@@ -2,17 +2,21 @@
 	<Modal v-model="visiable" :mask-closable="false" scrollable footer-hide>
 		<template slot="header">
 			<Icon type="ios-paper"/>
-			<span>查看用户</span>
+			<span>查看菜单</span>
 		</template>
 		<CellGroup>
-			<Cell title="姓名" label="name" :extra="form.name"/>
-			<Cell title="年龄" label="age" :extra="form.age+''"/>
-			<Cell title="地址" label="address" :extra="form.address"/>
-			<Cell title="创建者" label="creator" :extra="form.creator"/>
+			<Cell title="名称" label="title" :extra="form.title"/>
+			<Cell title="代码" label="code" :extra="form.code"/>
+			<Cell title="路由" label="router" :extra="form.router" :to="'/#'+form.router" target="_blank"/>
+			<Cell title="图标" :label="form.icon">
+				<template slot="extra">
+					<Icon :type="form.icon"/>
+				</template>
+			</Cell>
+			<Cell title="创建者" label="creator" :extra="form.creatorUsername"/>
 			<Cell title="创建时间" label="createDate" :extra="form.createDate"/>
-			<Cell title="最后更新者" label="updator" :extra="form.updator"/>
+			<Cell title="最后更新者" label="updator" :extra="form.updatorUsername"/>
 			<Cell title="最后更新时间" label="updateDate" :extra="form.updateDate"/>
-			<Cell title="最后登陆时间" label="loginDate" :extra="form.loginDate"/>
 		</CellGroup>
 	</Modal>
 </template>
