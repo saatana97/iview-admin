42349b953628cfe224a44f52b6d1570577177b89
saatana <1041367524@qq.com>
2019-03-22 15:13:45 +0800

    增加日期工具类

---
+++ src/scripts/DateUtils.js
@@ -0,0 +1,38 @@
+export default {
+  Format(date, format) {
+    let res = '';
+    if (date instanceof Date) {
+      format = format || 'yyyy-MM-dd HH:mm:ss';
+      res = format;
+      const opts = {
+        'y+': date => {
+          return date.getFullYear();
+        },
+        'M+': date => {
+          return date.getMonth() + 1;
+        },
+        'd+': date => {
+          return date.getDate();
+        },
+        'H+': date => {
+          return date.getHours();
+        },
+        'm+': date => {
+          return date.getMinutes();
+        },
+        's+': date => {
+          return date.getSeconds();
+        }
+      };
+      for (let pattern in opts) {
+        let reg = new RegExp(pattern, 'g');
+        let value = opts[pattern](date);
+        if (value < 10) {
+          value = '0' + value;
+        }
+        res = res.replace(reg, value);
+      }
+    }
+    return res;
+  }
+};
